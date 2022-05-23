3098e4d2e39f7f52415d59fcb0f5da58b200bb7d
Danielpeddersen <91453260+Danielpeddersen@users.noreply.github.com>
2022-05-23 09:05:29 +0200

    pygame del 3

--- snake_o4.py
+++ snake_o4.py
@@ -1,6 +1,8 @@
 import pygame # importerer pygame modulen
 
 pygame.init() # aktiverer funksjoner i pygame
+clock = pygame.time.Clock()
+fps = 4
 
 WHITE = (255,255,255)
 GREEN = (0,255,0)
@@ -13,31 +15,44 @@ HEIGHT = CELL_SIZE * CELL_NUMBER # høyden på vindu som vi finner ved å fange 
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT)) # oppretter spillervinduet
 
-slange = [7 * CELL_SIZE, 7 * CELL_SIZE, CELL_SIZE, CELL_SIZE] # 
-def draw_snake():
-    pygame.draw.rect(screen, (GREEN), slange)
+slange = [7 * CELL_SIZE, 7 * CELL_SIZE, CELL_SIZE, CELL_SIZE] # grønn firkant = slange
+dir = "right"
 
-def draw_cells():
-    for x in range(CELL_NUMBER):
-        pygame.draw.line(screen, (0,0,0), (x * CELL_SIZE,0), (x * CELL_SIZE,HEIGHT))
+def draw_snake(): #funskjon for å tegne slangen på spille brettet
+    pygame.draw.rect(screen, (GREEN), slange) # pygame sin funksjon for å tegne en firkant(rect)
+    if dir == "right":
+        slange[0] += 1 * CELL_SIZE
+    elif dir == "left":
+        pass
+
+def draw_cells(): # funksjon for å tegne cellene
+    for x in range(CELL_NUMBER): # en for løkke som går femten gang(cell_number)
+        pygame.draw.line(screen, (0,0,0), (x * CELL_SIZE,0), (x * CELL_SIZE,HEIGHT)) # pygame sin funskjon for å tegne en linje, linjen flytter seg CELL_SIZE til høyre for hver runde i løkken
     for y in range(CELL_NUMBER):
-        pygame.draw.line(screen, (0,0,0), (0 , y * CELL_SIZE), (WIDTH, y * CELL_SIZE))
+        pygame.draw.line(screen, (0,0,0), (0 , y * CELL_SIZE), (WIDTH, y * CELL_SIZE)) # tegner linjer som går vannrett, flytter seg selv CELL_SIZE ned for hver runde.
 
 
-def update():
-    screen.fill(WHITE)
-    draw_cells()
-    draw_snake()
-    pygame.display.update()
+def update(): # en funksjon som oppdaterer spillevinduet
+    screen.fill(WHITE) # fyller spillevinduet med hvit farge
+    draw_cells() # kjører draw_cells funskjonen
+    draw_snake() # kjører draw_snake funksjonen
+    pygame.display.update() # pygame funksjon for å opppdatere spillevinduet
 
 
-main = True
-while main: 
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                main = False
-    update()
+main = True # varibel som innholder boolien som innholder veriden True
+while main: # while løkke som går så lenge main variablen er sann.
+    clock.tick(fps)
+    for event in pygame.event.get(): # for løkke som sjekker pygame event
+        if event.type == pygame.KEYDOWN: # en if setning som sjekker om vi har trykket på tastaturet
+            if event.key == pygame.K_ESCAPE: # hvis man har trykket på ESC knappen
+                main = False # da setter den main til ufalsk
+            if event.key == pygame.K_LEFT:
+                dir = "left"
+            elif event.key == pygame.K_RIGHT:
+                dir = "right"
+                slange[1] += 1 * CELL_SIZE
+    update() # kaller på den update funksjonen som oppdaterer spillevinduet.
+
 
 
 
